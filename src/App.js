17b0bbb48d0b61475f7dfb3990b2fab9be2dd4a0
zubairahsan <zubairashan202@gmail.com>
2019-01-03 14:20:05 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Athlete1 from './assignment1/athlete';
+import FilterAthletes from './assignment2/filter';
+import Athlete from './assigment3/filterAndMap';
 class App extends Component {
   render() {
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+
+     
+        <Athlete1 />
+        <FilterAthletes />
+        <Athlete />
       </div>
     );
   }
---
+++ src/assignment1/athlete.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Athlete extends Component {
+  render() {
+    const athleteData = [
+      { athlete: 'Lionel Messi', team: 'Barcelona', income: 40000000 },
+      { athlete: 'Cristiano Ronaldo', team: 'Juventus', income: 30000000 },
+      { athlete: 'Neymar', team: 'Paris Saint-Germain', income: 36800000 },
+      { athlete: 'Eden Hazard', team: 'Chelsea', income: 10400000 },
+      { athlete: 'Mohamed Salah', team: 'Liverpool', income: 4680000 },
+      { athlete: 'Kylian Mbappé', team: 'Paris Saint-Germain: An American Musical', income: 17500000 },
+      { athlete: 'Luka Modrić', team: 'Real Madrid', income: 9360000 },
+      { athlete: 'Harry Kane', team: 'Tottenham Hotspurs', income: 17600000 },
+      { athlete: 'Kevin De Bruyne', team: 'Manchester City', income: 5980000 },
+      { athlete: 'Paul Pogba', team: 'Manchester United', income: 15080000 }
+    ];
+    const athleteIncomeStrings = athleteData.map((item, index) => {
+      return (
+        `${item.athlete} in ${item.team} earned ${item.income} Euros`
+      )
+    })
+    console.log(athleteIncomeStrings);
+    return (
+      <div>
+
+      
+      </div>
+    );
+  }
+}
+
+export default Athlete;
